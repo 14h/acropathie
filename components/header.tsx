11baa71c010acd27b705a747ed3e68b28c8135a9
Abdallah Kawji <contact@abdallah.dev>
2022-03-21 12:13:49 +0100

    [feat]: change styles + add eva related navigation

--- components/header.tsx
+++ components/header.tsx
@@ -3,57 +3,46 @@ import { useState } from 'react';
 import Link from 'next/link';
 
 type TProps = {
-    selected: 'about' | 'home' | 'services' | 'news' | 'results';
+    selected: 'about' | 'home' | 'workshops' | 'references' | 'gallery';
 }
-{/*<Link href='/spectra'>*/}
-{/*    <div className={`p-8 transition-all ease-in duration-75 ض hover:text-accent-8 cursor-pointer text-xl ${selected === 'spectra' ? 'text-accent-8' : ''}`}>*/}
-{/*        Spectra*/}
-{/*    </div>*/}
-{/*</Link>*/}
 
 export const Header = ({selected}: TProps) => {
     const [open, setOpen] = useState<boolean>(false);
 
     return (
-        <div className='bg-accent-4 px-4 lg:px-16 text-white transition-all ease-in duration-75'>
-            <div className='py-4 flex justify-between'>
-                <Link href='/'>
-                    <Image src="/icon.png" alt="kykeon Logo" width={180} height={80} />
-                </Link>
+        <div className='bg-white px-4 lg:px-16 transition-all ease-in duration-75'>
+            <div className='py-4 flex justify-center'>
                 <div className='flex flex-row justify-center items-center hidden lg:flex'>
                     <Link href='/'>
                         <div className={`p-8 transition-all ease-in duration-75 hover:text-accent-8 cursor-pointer text-xl ${selected === 'home' ? 'text-accent-8' : ''}`}>
-                            Home
+                            ACROPATHIE
                         </div>
                     </Link>
-                    <Link href='/services'>
-                        <div className={`p-8 transition-all ease-in duration-75 hover:text-accent-8 cursor-pointer text-xl ${selected === 'services' ? 'text-accent-8' : ''}`}>
-                            Our services
+
+                    <Link href='/workshops'>
+                        <div className={`p-8 transition-all ease-in duration-75 hover:text-accent-8 cursor-pointer text-xl ${selected === 'workshops' ? 'text-accent-8' : ''}`}>
+                            Workshops
                         </div>
                     </Link>
+
+                    <Link href='/references'>
+                        <div className={`p-8 transition-all ease-in duration-75 hover:text-accent-8 cursor-pointer text-xl ${selected === 'references' ? 'text-accent-8' : ''}`}>
+                            References
+                        </div>
+                    </Link>
+
+                    <Link href='/gallery'>
+                        <div className={`p-8 transition-all ease-in duration-75 hover:text-accent-8 cursor-pointer text-xl ${selected === 'gallery' ? 'text-accent-8' : ''}`}>
+                            Gallery
+                        </div>
+                    </Link>
+
                     <Link href='/about'>
                         <div className={`p-8 transition-all ease-in duration-75 hover:text-accent-8 cursor-pointer text-xl ${selected === 'about' ? 'text-accent-8' : ''}`}>
-                            Who we are
-                        </div>
-                    </Link>
-                    <Link href='/news'>
-                        <div className={`p-8 transition-all ease-in duration-75 hover:text-accent-8 cursor-pointer text-xl ${selected === 'news' ? 'text-accent-8' : ''}`}>
-                            News
-                        </div>
-                    </Link>
-                    <Link href='/results'>
-                        <div className={`p-8 transition-all ease-in duration-75 hover:text-accent-8 cursor-pointer text-xl ${selected === 'results' ? 'text-accent-8' : ''}`}>
-                            Results
+                            About Eva
                         </div>
                     </Link>
                 </div>
-                <Link href='mailto:info@kykeonanalytics.com'>
-                    <div
-                        className='px-8 py-4 my-4 hidden lg:block bg-accent-8 text-black font-bold transition-all duration-75 ease-in hover:bg-accent-9 cursor-pointer text-xl shadow-xl rounded-lg'
-                    >
-                        CONTACT US
-                    </div>
-                </Link>
                 <div className='lg:hidden py-4'>
                     <input onChange={() => setOpen(!open)} id="toggle" type="checkbox" />
                     <label className="hamburger" htmlFor="toggle">
@@ -68,34 +57,31 @@ export const Header = ({selected}: TProps) => {
                     <div className='flex flex-col justify-center items-center'>
                         <Link href='/'>
                             <div className={`p-8 transition-all ease-in duration-75 hover:text-accent-8 cursor-pointer text-xl ${selected === 'home' ? 'text-accent-8' : ''}`}>
-                                Home
+                                ACROPATHIE
                             </div>
                         </Link>
-                        <Link href='/services'>
-                            <div className={`p-8 transition-all ease-in duration-75 hover:text-accent-8 cursor-pointer text-xl ${selected === 'services' ? 'text-accent-8' : ''}`}>
-                                Our services
+
+                        <Link href='/workshops'>
+                            <div className={`p-8 transition-all ease-in duration-75 hover:text-accent-8 cursor-pointer text-xl ${selected === 'workshops' ? 'text-accent-8' : ''}`}>
+                                Workshops
                             </div>
                         </Link>
+
+                        <Link href='/references'>
+                            <div className={`p-8 transition-all ease-in duration-75 hover:text-accent-8 cursor-pointer text-xl ${selected === 'references' ? 'text-accent-8' : ''}`}>
+                                References
+                            </div>
+                        </Link>
+
+                        <Link href='/gallery'>
+                            <div className={`p-8 transition-all ease-in duration-75 hover:text-accent-8 cursor-pointer text-xl ${selected === 'gallery' ? 'text-accent-8' : ''}`}>
+                                Gallery
+                            </div>
+                        </Link>
+
                         <Link href='/about'>
                             <div className={`p-8 transition-all ease-in duration-75 hover:text-accent-8 cursor-pointer text-xl ${selected === 'about' ? 'text-accent-8' : ''}`}>
-                                Who we are
-                            </div>
-                        </Link>
-                        <Link href='/news'>
-                            <div className={`p-8 transition-all ease-in duration-75 hover:text-accent-8 cursor-pointer text-xl ${selected === 'news' ? 'text-accent-8' : ''}`}>
-                                News
-                            </div>
-                        </Link>
-                        <Link href='/results'>
-                            <div className={`p-8 transition-all ease-in duration-75 hover:text-accent-8 cursor-pointer text-xl ${selected === 'results' ? 'text-accent-8' : ''}`}>
-                                Results
-                            </div>
-                        </Link>
-                        <Link href='mailto:info@kykeonanalytics.com'>
-                            <div
-                                className='px-8 py-4 my-4 bg-accent-8 text-black font-bold transition-all duration-75 ease-in hover:bg-accent-9 cursor-pointer text-xl shadow-xl rounded-lg'
-                            >
-                                CONTACT US
+                                About Eva
                             </div>
                         </Link>
                         <br/>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,33 +1,15 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import { useState } from 'react';
 import { Header } from '../components/header';
 import { Footer } from '../components/footer';
-import Image from 'next/image';
 import { Animated } from '../components/animated';
 
-
-//      <footer className={styles.footer}>
-//         <a
-//           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Powered by{' '}
-//           <span className={styles.logo}>
-//             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-//           </span>
-//         </a>
-//       </footer>
-//     </div>
-//   )
-// }
 const Home: NextPage = () => {
     return (
         <>
             <Head>
-                <title>Kykeon Analytics</title>
-                <meta name="description" content="Kykeon Analytics"/>
+                <title>ACROPATHIE</title>
+                <meta name="description" content="ACROPATHIE"/>
                 <link rel="icon" href="/favicon.ico"/>
             </Head>
 
@@ -42,20 +24,6 @@ const Home: NextPage = () => {
                         <br/>
                         <Animated delay={75} className='w-48 border-2 border-solid border-accent-8'/>
                         <br/>
-                    </div>
-                    <div className='flex flex-col lg:flex-row justify-between h-full px-8 lg:px-16'>
-                        <Animated delay={300} className='font-thin text-2xl'>
-                            Kykeon Analytics is a public service initiative providing analytical harm reduction solutions in the field of newly emerging chemical entities.
-                            <br/>Our aim us is to create quality analytical spectra of novel chemical agents that have yet to be studied and scheduled,
-                            <br/>in order to characterize them and to create analytical spectra of these emerging compounds.
-                            <br/>Our goal is to provide frontline harm reduction organization, forensic scientists and researchers with the tools they need to save lives and advance our understanding of science.
-                            <br/>Our services are available free of charge as a public service to qualified organizations including,
-                            <br/>harm reduction organizations, academic  researchers, forensic scientists, as well as frontline medical teams.
-                        </Animated>
-                        <Animated delay={75} className='hidden lg:block'>
-                            <Image src="/kykeon_icon.png" alt="kykeon Logo" width={580} height={500} />
-                        </Animated>
-
                     </div>
                     <br/>
                     <br/>
